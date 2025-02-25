dc01b5f1c7f7f857f48b8c3e929e5c5d519524c9
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-25 10:46:27 -0300

    Add files via upload

---
+++ telaLogin/telaLogin/Login.cs
@@ -0,0 +1,44 @@
+namespace telaLogin;
+
+public partial class Login : Form
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void pictureBox1_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label1_Click_1(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label5_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label2_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}

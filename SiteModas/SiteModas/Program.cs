cd6901a23d4ae2b278dd5be583d2a47bad012fdc
Matheus <matheusfffcorrea@gmail.com>
2025-02-25 07:38:06 -0300

    Tela de login v1.0.0

---
+++ SiteModas/SiteModas/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Kenia Teixeira
+ * Data: 24/02/2025
+ * Hora: 09:06
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace SiteModas
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
